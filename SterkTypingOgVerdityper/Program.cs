b819dd20b8fa5e5d42a72743036bfbd3bf6a97aa
Lonatri <gyda-94@hotmail.com>
2025-11-17 14:43:10 +0100

    Lagde variabler

--- SterkTypingOgVerdityper/Program.cs
+++ SterkTypingOgVerdityper/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int tall = 4;
+long langtTall = 50500l;
+float desimalTall = 3.314f; //mindre enn double, krever mindre (mindre plass)
+decimal mangeDesimaler = 3.1415926536m; //større enn double (mer bytes)
+double desimalTall2 = 3.141; //default så trenger ikke d bak (mellom float og decimal, tar litt mer plass enn float)
+
+string text = "This is a text";
+char character = 'a';
+bool thisIsTrue = true;
